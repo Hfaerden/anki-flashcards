ecfeb63bac10bef8ab39e1d906175fbd553b2377
Fuchsia <havardfarden@gmail.com>
2025-03-21 21:40:28 +0100

    IT ACTUALLY WORKS SOMEHOW

--- refreshfiles.py
+++ refreshfiles.py
@@ -1,20 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
-import html2text
 url = "https://www.worldcubeassociation.org/regulations/full/"
 
 #clears files to reprint all regs and guidelines
 open("regulations.csv", "w").close()
 open("guidelines.csv", "w").close()
-open("test.txt", "w").close()
+open("regs.txt", "w").close()
 
 #updates regulations in regs.txt
 nightmarelistwithalltheregs = BeautifulSoup(requests.get(url).text).get_text().split("\n")
-print(nightmarelistwithalltheregs)
 
-with open("test.txt", "a", encoding="utf-8") as f:
+with open("regs.txt", "a", encoding="utf-8") as f:
     for line in nightmarelistwithalltheregs:
-        if (")" in line) or ("Article" in line and line.startswith(" ")):
+        if (not line.startswith("For")) and ((")" in line) or ("Article" in line and line.startswith(" "))):
             f.write(line + "\n")
 
 #Guidelines
@@ -22,7 +20,7 @@ with open("regs.txt", "r") as f:
     with open("guidelines.csv", "a") as g:
         all = f.readlines()
         for reg in all:
-            if reg.startswith("Article"):
+            if reg.startswith(" Article"):
                 currentarticle = reg.split(":")[0].replace(" ", "_")
             if (reg.split(")")[0]).endswith("+"):
                 justreg = reg.split(")")
@@ -34,7 +32,7 @@ with open("regs.txt", "r") as f:
     with open("regulations.csv", "a") as g:
         all = f.readlines()
         for reg in all:
-            if reg.startswith("Article"):
+            if reg.startswith(" Article"):
                 currentarticle = reg.split(":")[0].replace(" ", "_")
                 continue
             if not (reg.split(")")[0]).endswith("+"):
